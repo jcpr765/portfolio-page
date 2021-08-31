97724254a33192e7c4dfced244ef6dcef75a0bff
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-30 20:40:23 -0400

    Separate data into separate arrays for each column

---
+++ resumeData.js
@@ -0,0 +1,120 @@
+export const leftColumnData = [
+  {
+    title: "Personal Profile",
+    records: [
+      {
+        details: ["Puerto Rican Full Stack developer, CS graduate"],
+        displayAsList: false,
+      },
+    ],
+  },
+  {
+    title: "Contact Details",
+    records: [
+      {
+        details: [
+          "juan.c.morales.personal@gmail.com",
+          "www.linkedin.com/in/jmoralesleduc",
+        ],
+        displayAsList: false,
+      },
+    ],
+  },
+  {
+    title: "Academic Profile",
+    records: [
+      {
+        details: [
+          "Magna Cum Laude 3.92 GPA",
+          "Board Member of local ACM student chapter (2015-2017)",
+        ],
+        role: "Bachelors in Computer Science | 2013 - 2018",
+        employer: "University of Puerto Rico Bayamón",
+      },
+    ],
+  },
+  {
+    title: "Skills and Abilities",
+    records: [
+      {
+        details: [
+          "Programming languages: JavaScript, Python, Java",
+          "Frameworks: React / NextJS, Django, NodeJS, Redux",
+          "AWS experience with ELBs",
+          "Version Control experience with Git",
+          "Agile development experience",
+          "Bilingual (English and Spanish)",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Other Skills and Accomplishments",
+    records: [
+      {
+        details: [
+          "1st Place Startup Weekend (2015) competition",
+          "2nd Place regional competitive programming competitions (2014)",
+          "Puerto Rican representative for the ACM ICPC international programming competitions (2014-2016)",
+          "2nd Place in the Puerto Rico Game Jam (2015)",
+        ],
+      },
+    ],
+  },
+];
+
+export const rightColumnData = [
+  {
+    title: "Employment History",
+    records: [
+      {
+        role: "Software Engineer",
+        employer: "CareLinx | May 2018 - Present",
+        details: [
+          "Creating various internal dashboards and tools for many company initiatives.",
+          "Developing integrations for various company partnerships (LMS, Insurance, Employee benefits, Intercom)",
+          "Creating responsive promotional and marketing pages",
+          "Full Stack work with React and Django",
+        ],
+      },
+      {
+        role: "Software Engineer",
+        employer: "Awesound | Feb - May 2018",
+        details: [
+          "Developed a Feed Finder tool, for promoting the platform to podcasts of specific categories, using React",
+          "Implemented queueing functionality with Celery and RabbitMQ",
+          "Developed charts and other visuals for podcast owner statistics like listeners per country, using Python and JavaScript.",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Internships",
+    records: [
+      {
+        role: "Researcher",
+        employer: "University of South Florida | May - July 2016",
+        details: [
+          "Indoor localization using Wi-Fi Technology and Bluetooth LowEnergy (BLE) Beacons. Android development with Java.",
+        ],
+        displayAsList: false,
+      },
+      {
+        role: "Software Development Intern",
+        employer: "KPG | January - May 2016",
+        details: [
+          "Worked on implementation of SCORM compliance for an e-learning platform. C# and JavaScript with .NET Framework",
+        ],
+        displayAsList: false,
+      },
+      {
+        role: "Researcher",
+        employer: "MIT Haystack Observatory | May - July 2013",
+        details: [
+          "Developing a platform for visualization of VLBI sessions for radio observatories. JavaScript and Python.",
+        ],
+        displayAsList: false,
+      },
+    ],
+  },
+];
