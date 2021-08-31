4cdd76edab39aa1fea8cd9befe45f16c5dc9c920
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-30 20:40:50 -0400

    use split data

--- components/ResumeInfo.js
+++ components/ResumeInfo.js
@@ -1,14 +1,38 @@
 import React from "react";
 import ResumeSection from "./ResumeSection";
-import resumeData from "../resumeData.json";
+import { leftColumnData, rightColumnData } from "../resumeData";
 
 const ResumeInfo = () => {
   return (
-    <div>
-      {resumeData.map((section, idx) => (
-        <ResumeSection key={idx} {...section} />
-      ))}
-      <style></style>
+    <div className="resume-info">
+      <div className="left-column">
+        {leftColumnData.map((section, idx) => (
+          <ResumeSection key={idx} {...section} />
+        ))}
+      </div>
+      <div className="right-column">
+        {rightColumnData.map((section, idx) => (
+          <ResumeSection key={idx} {...section} />
+        ))}
+      </div>
+      <style jsx>{`
+        .resume-info {
+          display: flex;
+        }
+
+        .left-column,
+        .right-column {
+          flex: 1;
+        }
+
+        .left-column > div {
+          margin: 0 0 0 auto;
+        }
+
+        .right-column > div {
+          margin: 0 auto 0 0;
+        }
+      `}</style>
     </div>
   );
 };
