b6d2d69ad5bc3eee2b107880c68641dc6f26f89a
Juan Carlos Morales <11338763+jcpr765@users.noreply.github.com>
2021-08-31 22:01:50 -0400

    add jsx tag

--- components/ResumeHeader.js
+++ components/ResumeHeader.js
@@ -9,39 +9,39 @@ const ResumeHeader = ({ name, title }) => {
       <span>{name}</span>
       <h3>{title}</h3>
       <div className="bottom-section" />
-      <style>
+      <style jsx>
         {`
-        .title-header {
+          .title-header {
             display: flex;
             flex-direction: column;
             align-items: center;
             text-align: center;
-        }
+          }
 
-        .top-section {
+          .top-section {
             background-color: ${MAIN_COLORS.BLUE};
             height: 3.125rem;
             width: 100vw;
             margin-bottom: 3.125rem;
-        }
+          }
 
-        .bottom-section {
+          .bottom-section {
             background-color: ${MAIN_COLORS.BLUE};
             height: 5px;
             width: 70%;
             margin-bottom: 3.125rem;
-        }
+          }
 
-        span {
+          span {
             font-weight: 600;
             font-size: 3rem;
-        }
+          }
 
-        h3 {
+          h3 {
             font-weight: 300;
             font-size: 2rem;
-        }
-          `}
+          }
+        `}
       </style>
     </header>
   );
